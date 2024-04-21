884fc461f6bb1d352fd5b665568de93e0aaed7d8
Pranav Upadhyay <62388521+pranavupadhyay123@users.noreply.github.com>
2024-04-21 17:55:10 +0530

    version from 2 to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "co.pranavlabs.quietzone"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("release")
